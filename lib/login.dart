e17129273479c65ee331b60022b0080250f55c4c
Aman Agrawal <amanagrawal9876@gmail.com>
2024-01-26 16:28:29 +0530

    update changes

--- lib/login.dart
+++ lib/login.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:greenwealth/home.dart';
 
-import 'home.dart';
 
 class login extends StatefulWidget {
   const login({super.key});
@@ -13,132 +13,323 @@ class _loginState extends State<login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       body: Container(
-          height: MediaQuery. of(context). size. height,
-          width: MediaQuery. of(context). size. width,
-          decoration: BoxDecoration(
-            color: Color(0xFFF52646),
-          ),
-        child:Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        SizedBox(height: 50,),
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            IconButton(onPressed: (){Navigator.pop(context);}, icon: Icon(Icons.arrow_back_outlined,color: Colors.white,size: 30,)),
-            SizedBox(width: 130,),
-            Image.asset("images/Drop 2.png",height: 50,),
-          ],
-
-        ),
-        SizedBox(height: 80,),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text("Login to \nyour account",style: TextStyle(color:Colors.white,fontWeight: FontWeight.bold,fontSize: 40),),
-        ),
-        SizedBox(height: 20,),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
+        body: Container(
+            height: MediaQuery. of(context). size. height,
+            width: MediaQuery. of(context). size. width,
             decoration: BoxDecoration(
-              color:Colors.white,
-              borderRadius:BorderRadius.circular(20),
-
+              color: Colors.white,
             ),
-            width: 400,
-            height: 60,
-            child: Center(child: Text("Continue with Google"))
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
+            child:SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(height: 50,),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      IconButton(onPressed: (){Navigator.pop(context);}, icon: Icon(Icons.arrow_back_outlined,color: Colors.white,size: 30,)),
+                      SizedBox(width: 130,),
+                    ],
+                  ),
+                  SizedBox(height: 20,),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                        decoration: BoxDecoration(
+                          color:Colors.grey[300],
+                          borderRadius:BorderRadius.circular(20),
+              
+                        ),
+                        width: 400,
+                        height: 60,
+                        child: Center(child: Text("Continue with Google",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18),))
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+              
+              
+                        width: 400,
+                        height: 60,
+                        child:Center(child: Text("------------------------------------------ or------------------------------------------",style: TextStyle(color: Colors.black),))
+              
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color:Colors.grey[200],
+                        borderRadius:BorderRadius.circular(20),
+              
+                      ),
+              
+                      height: 60,
+                      child: TextFormField(
+              
+                        decoration: InputDecoration(labelText: 'Enter your email',
+                          prefixIcon: Icon(Icons.mail_outline_outlined),
+              
+                          border: OutlineInputBorder(
+                              borderRadius: const BorderRadius.all(
+              
+                                  Radius.circular(20.0))
+                          ),),
+                        validator: (input) => input!.trim().isEmpty
+                            ? 'Please enter a valid name'
+                            : null,
+                        //    onSaved: (input) => phone = input!,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color:Colors.grey[200],
+                        borderRadius:BorderRadius.circular(20),
+              
+                      ),
+              
+                      height: 60,
+                      child: TextFormField(
+              
+                        decoration: InputDecoration(labelText: 'Enter your password',
+                          prefixIcon: Icon(Icons.key),
+              
+                          border: OutlineInputBorder(
+                              borderRadius: const BorderRadius.all(
+              
+                                  Radius.circular(20.0))
+                          ),),
+                        validator: (input) => input!.trim().isEmpty
+                            ? 'Please enter a valid name'
+                            : null,
+                        //  onSaved: (input) => password = input!,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                    child: Container(
+                      height: 50,
+                      width: 370,
+                      decoration: BoxDecoration(
+                          color: Colors.green,
+                          borderRadius: BorderRadius.circular(20)
+                      ),
+                      child: Center(child: TextButton(
+                          onPressed: (){
+                            Navigator.push(context, MaterialPageRoute(builder: (context) =>  homepage()),);
 
 
-            width: 400,
-            height: 60,
-            child:Center(child: Text("------------------------------------------ or------------------------------------------",style: TextStyle(color: Colors.white),))
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            decoration: BoxDecoration(
-              color:Colors.white,
-              borderRadius:BorderRadius.circular(20),
 
-            ),
+                          },
+                          child: Text("Login ",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,color: Colors.white),))),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
 
-            height: 60,
-            child: TextFormField(
-
-              decoration: InputDecoration(labelText: 'email id',
-                prefixIcon: Icon(Icons.mail_outline_outlined),
-
-                border: OutlineInputBorder(
-                    borderRadius: const BorderRadius.all(
-
-                        Radius.circular(20.0))
-                ),),
-              validator: (input) => input!.trim().isEmpty
-                  ? 'Please enter a valid name'
-                  : null,
-          //    onSaved: (input) => phone = input!,
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            decoration: BoxDecoration(
-              color:Colors.white,
-              borderRadius:BorderRadius.circular(20),
-
-            ),
-
-            height: 60,
-            child: TextFormField(
-
-              decoration: InputDecoration(labelText: 'Password',
-                prefixIcon: Icon(Icons.key),
-
-                border: OutlineInputBorder(
-                    borderRadius: const BorderRadius.all(
-
-                        Radius.circular(20.0))
-                ),),
-              validator: (input) => input!.trim().isEmpty
-                  ? 'Please enter a valid name'
-                  : null,
-            //  onSaved: (input) => password = input!,
-            ),
-          ),
-        ),
-        SizedBox(
-          height: 100,
-        ),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
-          child: Container(
-            height: 50,
-            width: 370,
-            decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(20)
-            ),
-            child: Center(child: TextButton(
-                onPressed: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context) =>  homescreen()),);
-
-                },
-                child: Text("continue ",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),))),
-          ),
-        ),
+                  TextButton(onPressed: (){}, child: Text("Forgot Password?",style: TextStyle(fontSize:15,color: Colors.black),)),
+                  SizedBox(height: 50,),
+                  Divider(),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Don't have an account?"),
+                  TextButton(onPressed: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (context) =>  signup()),);
 
 
-      ],))
+                  }, child: Text("Signup",style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold),))
+                ],
+              )
+                ],),
+            ))
     );
 
+  }
+}
+
+class signup extends StatefulWidget {
+  const signup({super.key});
+
+  @override
+  State<signup> createState() => _signupState();
+}
+
+class _signupState extends State<signup> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+            height: MediaQuery. of(context). size. height,
+            width: MediaQuery. of(context). size. width,
+            decoration: BoxDecoration(
+              color: Colors.white,
+            ),
+            child:SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(height: 50,),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      IconButton(onPressed: (){Navigator.pop(context);}, icon: Icon(Icons.arrow_back_outlined,color: Colors.white,size: 30,)),
+                      SizedBox(width: 130,),
+                    ],
+                  ),
+                  SizedBox(height: 20,),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                        decoration: BoxDecoration(
+                          color:Colors.grey[300],
+                          borderRadius:BorderRadius.circular(20),
+
+                        ),
+                        width: 400,
+                        height: 60,
+                        child: Center(child: Text("Continue with Google",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18),))
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+
+
+                        width: 400,
+                        height: 60,
+                        child:Center(child: Text("------------------------------------------ or------------------------------------------",style: TextStyle(color: Colors.black),))
+
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color:Colors.grey[200],
+                        borderRadius:BorderRadius.circular(20),
+
+                      ),
+
+                      height: 60,
+                      child: TextFormField(
+
+                        decoration: InputDecoration(labelText: 'Enter your email',
+
+                          border: OutlineInputBorder(
+                              borderRadius: const BorderRadius.all(
+
+                                  Radius.circular(20.0))
+                          ),),
+                        validator: (input) => input!.trim().isEmpty
+                            ? 'Please enter a valid name'
+                            : null,
+                        //    onSaved: (input) => phone = input!,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color:Colors.grey[200],
+                        borderRadius:BorderRadius.circular(20),
+
+                      ),
+
+                      height: 60,
+                      child: TextFormField(
+
+                        decoration: InputDecoration(labelText: 'Enter your password',
+
+                          border: OutlineInputBorder(
+                              borderRadius: const BorderRadius.all(
+
+                                  Radius.circular(20.0))
+                          ),),
+                        validator: (input) => input!.trim().isEmpty
+                            ? 'Please enter a valid name'
+                            : null,
+                        //  onSaved: (input) => password = input!,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color:Colors.grey[200],
+                        borderRadius:BorderRadius.circular(20),
+
+                      ),
+
+                      height: 60,
+                      child: TextFormField(
+
+                        decoration: InputDecoration(labelText: 'Enter your Full name',
+
+                          border: OutlineInputBorder(
+                              borderRadius: const BorderRadius.all(
+
+                                  Radius.circular(20.0))
+                          ),),
+                        validator: (input) => input!.trim().isEmpty
+                            ? 'Please enter a valid name'
+                            : null,
+                        //  onSaved: (input) => password = input!,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                    child: Container(
+                      height: 50,
+                      width: 370,
+                      decoration: BoxDecoration(
+                          color: Colors.green,
+                          borderRadius: BorderRadius.circular(20)
+                      ),
+                      child: Center(child: TextButton(
+                          onPressed: (){
+
+
+                          },
+                          child: Text("Signup ",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,color: Colors.white),))),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+
+                  SizedBox(height: 50,),
+                  Divider(),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Already have an account?"),
+                      TextButton(onPressed: (){
+                        Navigator.pop(context);
+                      }, child: Text("Signin",style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold),))
+                    ],
+                  )
+                ],),
+            ))
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
-import 'package:blooddonation/Signup.dart';
-import 'package:blooddonation/splashscreen.dart';
 import 'package:flutter/material.dart';
+
+import 'Login.dart';
 
 void main() {
   runApp(const MyApp());
 }
-String sinterest="";
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -16,11 +15,25 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red.shade900),
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const splashscreen(),
+      home: const login(),
     );
   }
 }
@@ -44,138 +57,58 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  TextEditingController searchinterest = TextEditingController();
+  int _counter = 0;
 
-  List<String> bloodgroup=["A+","A-","B+","B-","O+","O-","AB+","AB-"];
 
   @override
   Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.red.shade600,
-        title: Center(child: Text(widget.title,style: TextStyle(color: Colors.white),)),
-        leading:IconButton(onPressed: (){}, icon: Icon(Icons.menu,color: Colors.white,)),
-
-          actions: [
-          IconButton(onPressed: (){}, icon: CircleAvatar(backgroundImage: AssetImage("images/profile_pic.png"),radius: 20,)),
-        ],
-
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
       ),
-      body: Column(
-        children:[
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: SizedBox(
-              height: 60,
-              child: TextField(
-                controller: searchinterest,
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.search),
-                  border: OutlineInputBorder(
-                      borderRadius: const BorderRadius.all(
-                          Radius.circular(200.0))
-                  ),
-
-                  labelText: 'Search',
-                ),
-                onTap: () {
-                  setState(() async {
-                    sinterest = searchinterest.text;
-                  });
-                },
-                onSubmitted: (String str){
-                  sinterest=str;
-                },
-
-              ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          //
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
             ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          SizedBox(
-            width: 400,
-            height: 35,
-            child: ListView.builder(
-                itemCount: 8,
-                scrollDirection: Axis.horizontal,
-
-                itemBuilder: (BuildContext context,int index){
-                  return Row(
-                    children: [
-                      SizedBox(
-                        width: 20,
-                      ),
-                      Container(
-
-                        padding: EdgeInsets.all(8.0),
-                        decoration: BoxDecoration(
-                            color: Colors.red.shade600
-                        ),
-                        child: Text(bloodgroup[index],style: TextStyle(color:Colors.white),),
-                      ),
-                    ],
-                  );
-
-    } ,
-    ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(14.0),
-            child: Container(
-              height: 110,
-              width: 400,
-              decoration: BoxDecoration(
-                color: Colors.pink.shade200.withOpacity(0.3),
-                borderRadius:BorderRadius.all(Radius.circular(10.0))),
-              child: Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Image.asset("images/profile_pic.png"),
-                  ),
-                  Column(
-                    children: [
-
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(8,14,3,3),
-                        child: Text("Aman Agrawal",style: TextStyle(fontSize: 20),),
-                      ),
-                      Text("D-52/47,Luxa,"),
-                      Text("Varanasi"),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(75,15,8,0),
-                        child: Align(
-                          alignment: Alignment.topRight,
-                            child: Text("O+",style: TextStyle(fontSize: 28,fontWeight: FontWeight.bold,color: Colors.pink),)),
-                      ),
-                      Row(
-                        children: [
-                          IconButton(onPressed: (){
-                            Navigator.push(context, MaterialPageRoute(builder: (context) =>  signup()),);
-                          }, icon: Icon(Icons.message_rounded),style: IconButton.styleFrom(shape: CircleBorder()),)
-                         // ElevatedButton(onPressed: (){}, child:Icon(Icons.message_rounded),style: ElevatedButton.styleFrom(shape: CircleBorder()),),
-                          // ElevatedButton(onPressed: (){}, child:Icon(Icons.message_rounded),style: ElevatedButton.styleFrom(shape: CircleBorder()),),
-
-                        ],
-                      )
-
-
-                    ],
-                  ),
-
-                ],
-              ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
             ),
-          ),
-
-
-        ],
+          ],
+        ),
       ),
+
+
     );
   }
 }
